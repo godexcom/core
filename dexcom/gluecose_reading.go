c968c7a98f3b12c53b99b54a5d6822776b5cbbac
ar065 <nitsua0615@gmail.com>
2025-10-28 02:21:15 -0400

    Refactor GlucoseReading struct and method names for clarity and consistency

--- dexcom/gluecose_reading.go
+++ dexcom/gluecose_reading.go
@@ -3,27 +3,36 @@ package dexcom
 type GlucoseReading struct {
 	Value     int    `json:"Value"`
 	Trend     string `json:"Trend"`
-	WT        string `json:"WT"` // Wait Time
+	Date      string `json:"WT"` // Wait Time
 	TrendRate int    `json:"TrendRate"`
 }
 
+func NewGlucoseReading(value int, trend string, date string, trendRate int) *GlucoseReading {
+	return &GlucoseReading{
+		Value:     value,
+		Trend:     trend,
+		Date:      date,
+		TrendRate: trendRate,
+	}
+}
+
 // Helper methods for trend interpretation
-func (gr *GlucoseReading) TrendDirection() TrendDirection {
+func (gr *GlucoseReading) GetTrendDirection() TrendDirection {
 	return TrendDirectionNames[gr.Trend]
 }
 
-func (gr *GlucoseReading) TrendArrow() string {
-	return TrendArrows[gr.TrendDirection()]
+func (gr *GlucoseReading) GetTrendArrow() string {
+	return TrendArrows[gr.GetTrendDirection()]
 }
 
-func (gr *GlucoseReading) TrendDescription() string {
-	return TrendDescriptions[gr.TrendDirection()]
+func (gr *GlucoseReading) GetTrendDescription() string {
+	return TrendDescriptions[gr.GetTrendDirection()]
 }
 
-func (gr *GlucoseReading) ValueMMOL() float64 {
+func (gr *GlucoseReading) GetValueMMOL() float64 {
 	return float64(gr.Value) * MMOL_L_CONVERSION_FACTOR
 }
 
-func (gr *GlucoseReading) Date() int64 {
-	return parseDate(gr.WT)
+func (gr *GlucoseReading) GetDate() int64 {
+	return parseDate(gr.Date)
 }
